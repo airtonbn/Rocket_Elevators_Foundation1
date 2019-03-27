5a173811bfdc666038d93bc33e25673cc9b40d5a
samuelasselin <46904548+samuelasselin@users.noreply.github.com>
2019-03-27 15:10:17 -0400

    add map to admin panel 2.0

---
+++ app/admin/geolocalisation_admin.rb
@@ -0,0 +1,11 @@
+Trestle.admin(:geolocalisation) do
+    menu do
+      item :geolocalisation, icon: "fa fa-tachometer"
+    end
+  
+    controller do
+      def index
+       
+      end
+    end
+  end

47254f8f8989725249a77dc78401ce00599a2b79
Olivier Denis <olivier.denis1@gmail.com>
2019-03-29 08:37:10 -0400

    Working Dropbox

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,2 +1,47 @@
+require 'dropbox_api'
 class Lead < ApplicationRecord
+    belongs_to :customer, optional: true
+    def dropbox
+        #Connect to Dropbox with API Key
+        client = DropboxApi::Client.new(ENV['dropboxapi'])
+            
+        #Lead informations
+        content = self.file_attachment[0].tempfile
+        f_name = self.file_attachment[0].original_filename
+        b_name = self.business_name
+        the_time = Time.now
+        the_time.strftime "%Y-%m-%d %H:%M"
+
+        #Check if business name exist in customer table
+        c = Customer.find_by company_name: self.business_name
+            
+        #If extist
+        if c != nil
+            self.customer_id = c.id
+
+            # Check if there is a file attachment with the lead
+            if self.file_attachment[0].tempfile != nil
+
+                #Use a Begin/Rescue/End to catch the errors
+                #begin
+                    input = f_name
+                    values = input.split(".")
+                    #file = client.upload "/Company_Attachments/#{b_name}/#{values[0]}.#{values[1]}", content.read
+                    client.upload "/Company_Attachments/#{b_name}/#{values[0]}_[#{the_time.strftime "%Y-%m-%d %H:%M:%S"}].#{values[1]}", content.read
+                    puts "********************************************************************************************************" 
+
+                # rescue Exception
+                #     puts "********************************************************************************************************"
+                #     puts "ERROR DETECTED: Could not upload your files to Dropbox server."
+                #     puts "********************************************************************************************************" 
+                #     n = 1
+                #     while !client.upload("/Company_Attachments/#{b_name}/#{values[0]}(#{n}).#{values[1]}", content) do
+                #         n += 1
+                #     end
+                # end
+                self.file_attachment = nil
+                self.save!
+            end    
+        end
+    end
 end
--- db/migrate/20190314231443_create_leads.rb
+++ db/migrate/20190314231443_create_leads.rb
@@ -1,6 +1,7 @@
 class CreateLeads < ActiveRecord::Migration[5.2]
   def change
     create_table :leads do |t|
+      t.belongs_to :customer, index: true, foreign_key: {on_delete: :cascade, on_update: :cascade}
       t.string :full_name
       t.string :email
       t.string :phone

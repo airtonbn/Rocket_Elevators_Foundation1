bc8821b96f94c39478418a5ee95ce6b28f05f768
samuelasselin <46904548+samuelasselin@users.noreply.github.com>
2019-03-29 09:45:00 -0400

    Patch zendesk error

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,6 +2,29 @@ require 'dropbox_api'
 class Lead < ApplicationRecord
     after_create :send_notif_to_zendesk
     belongs_to :customer, optional: true
+
+    def send_notif_to_zendesk
+        if self.file_attachment
+            extra_text = "The Contact uploaded an attachment"
+        else
+            extra_text = ""
+        end
+
+        zendesklead(extra_text)
+    end
+    
+    def zendesklead(extra_text)
+        ZendeskAPI::Ticket.create!($client, 
+            :subject => "#{self.full_name} from #{self.business_name}", 
+            :comment => "The contact #{self.full_name} from company #{self.business_name} can be reached at email #{self.email} and at phone number #{self.phone}. 
+            #{self.department} has a project named #{self.project_name} which would require contribution from Rocket Elevators.
+            \n Project Description : #{self.project_description} 
+            \n Attached message : #{self.message}  
+            #{extra_text} ",
+            )
+        end
+    end
+    
     def dropbox
         #Connect to Dropbox with API Key
         client = DropboxApi::Client.new(ENV['dropboxapi'])
@@ -40,25 +63,4 @@ class Lead < ApplicationRecord
             end    
         end
 
-    def send_notif_to_zendesk
-        if self.file_attachment
-            extra_text = "The Contact uploaded an attachment"
-        else
-            extra_text = ""
-        end
-
-        zendesklead(extra_text)
-    end
-    
-    def zendesklead(extra_text)
-        ZendeskAPI::Ticket.create!($client, 
-            :subject => "#{self.full_name} from #{self.business_name}", 
-            :comment => "The contact #{self.full_name} from company #{self.business_name} can be reached at email #{self.email} and at phone number #{self.phone}. 
-            #{self.department} has a project named #{self.project_name} which would require contribution from Rocket Elevators.
-            \n Project Description : #{self.project_description} 
-            \n Attached message : #{self.message}  
-            #{extra_text} ",
-            )
-        end
-    end
 end
